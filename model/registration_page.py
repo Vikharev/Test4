7162ec157371cc76bd33148d6dafaaa7e21615b1
Alexey Vikharev <a.v.vikharev@gmail.com>
2025-04-05 15:41:00 +0300

    add allure

--- model/registration_page.py
+++ model/registration_page.py
@@ -1,3 +1,4 @@
+import allure
 import os
 
 from selene import browser, be, have
@@ -6,27 +7,35 @@ import tests
 
 
 class RegistrationPage:
+    @allure.step('Открытие страницы /automation-practice-form')
     def open(self):
         browser.open("/automation-practice-form")
 
+    @allure.step('Удаление мешающих банеров')
     def remove_banners(self):
         browser.element('footer').execute_script('element.remove()')
 
+    @allure.step('Заполнение имени значением: {value}')
     def fill_first_name(self, value):
         browser.element("#firstName").type(value)
 
+    @allure.step('Заполнение фамилии значением: {value}')
     def fill_last_name(self, value):
         browser.element("#lastName").type(value)
 
+    @allure.step('Заполнение E-MAIL значением: {value}')
     def fill_email(self, value):
         browser.element("#userEmail").type(value)
 
+    @allure.step('Выбор пола: {value}')
     def select_gender(self, value=None):
         browser.all("label").element_by(have.exact_text(value)).click()
 
+    @allure.step('Заполнение телефона значением: {value}')
     def fill_mobile_number(self, value):
         browser.element("#userNumber").type(value)
 
+    @allure.step('Заполнение даты рождения значением: {day}.{month}.{year}')
     def fill_date_of_birth(self, year, month, day):
         browser.element("#dateOfBirthInput").click()
         (browser.element(".react-datepicker__year-select").click().
@@ -35,14 +44,17 @@ class RegistrationPage:
          element(f'[value="{month - 1}"]').click())
         browser.element(f'.react-datepicker__day--0{day}').click()
 
+    @allure.step('Выбор предмета: {value}')
     def fill_subject(self, value):
         browser.element('#subjectsInput').type(value).press_enter()
 
+    @allure.step('Выбор хобби: {hobbies}')
     def select_hobbies(self, *hobbies):
         for hobby in hobbies:
             element = browser.all('[for^=hobbies-checkbox]').element_by(have.text(hobby))
             element.should(be.clickable).click()
 
+    @allure.step('Загрузка изображения: {value}')
     def upload_photo(self, value):
         browser.element("#uploadPicture").type(
             os.path.abspath(
@@ -51,19 +63,24 @@ class RegistrationPage:
             )
         )
 
+    @allure.step('Заполнение адреса: {value}')
     def fill_current_address(self, value):
         browser.element("#currentAddress").type(value)
 
+    @allure.step('Выбор штата: {value}')
     def fill_state(self, value):
         browser.element('#state').click().all("#state div").element_by(have.exact_text(value)).click()
 
+    @allure.step('Выбор города: {value}')
     def fill_city(self, value):
         browser.element('#city').click().all("#city div").element_by(have.exact_text(value)).click()
 
+    @allure.step('Нажатие на кнопку отправки формы')
     def click_submit_button(self):
         browser.element('#submit').should(be.visible).click()
 
     @staticmethod
+    @allure.step('Проверка корректной регистрации')
     def should_have_registered(
         full_name,
         email,
@@ -92,5 +109,6 @@ class RegistrationPage:
             )
         )
 
+    @allure.step('Нажатие на кнопку закрытия окна')
     def click_close_button(self):
         browser.element('#closeLargeModal').should(be.visible).click()
--- tests/test_submit_form.py
+++ tests/test_submit_form.py
@@ -1,37 +1,48 @@
+import allure
+
+from allure_commons.types import Severity
 from model.registration_page import RegistrationPage
 
 
+@allure.tag("testing Jenkins")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "Somebody")
+@allure.feature("QA-2025")
+@allure.story("GURU-10")
+@allure.link("https://github.com", name="Test demoqa")
+@allure.title('Проверка регистрации')
 def test_submit_form_success(browser_settings):
-    registration_page = RegistrationPage()
-    registration_page.open()
-    registration_page.remove_banners()
-
-    registration_page.fill_first_name("Иван")
-    registration_page.fill_last_name("Иванов")
-    registration_page.fill_email("ivanov@yandex.ru")
-    registration_page.select_gender("Male")
-    registration_page.fill_mobile_number("1234567890")
-    registration_page.fill_date_of_birth(1989, 11, 11)
-    registration_page.fill_subject("Maths")
-    registration_page.select_hobbies("Sports")
-    registration_page.upload_photo("20241021_190523.jpg")
-    registration_page.fill_current_address(
-        "Москва, ул.Ленина, д.1, кв.100"
-    )
-    registration_page.fill_state("NCR")
-    registration_page.fill_city("Delhi")
-
-    registration_page.click_submit_button()
-    registration_page.should_have_registered(
-        full_name="Иван Иванов",
-        email="ivanov@yandex.ru",
-        gender="Male",
-        phone="1234567890",
-        date_of_birth="11 November,1989",
-        subjects="Maths",
-        hobbies="Sports",
-        file_name="20241021_190523.jpg",
-        address="Москва, ул.Ленина, д.1, кв.100",
-        state="NCR",
-        city="Delhi",
-    )
+    with allure.step('Открыть форму для заполнения'):
+        registration_page = RegistrationPage()
+        registration_page.open()
+        registration_page.remove_banners()
+    with allure.step('Заполнить форму регистрации'):
+        registration_page.fill_first_name("Иван")
+        registration_page.fill_last_name("Иванов")
+        registration_page.fill_email("ivanov@yandex.ru")
+        registration_page.select_gender("Male")
+        registration_page.fill_mobile_number("1234567890")
+        registration_page.fill_date_of_birth(1989, 11, 11)
+        registration_page.fill_subject("Maths")
+        registration_page.select_hobbies("Sports")
+        registration_page.upload_photo("20241021_190523.jpg")
+        registration_page.fill_current_address(
+            "Москва, ул.Ленина, д.1, кв.100"
+        )
+        registration_page.fill_state("NCR")
+        registration_page.fill_city("Delhi")
+    with allure.step('Нажать кнопку Submit'):
+        registration_page.click_submit_button()
+        registration_page.should_have_registered(
+            full_name="Иван Иванов",
+            email="ivanov@yandex.ru",
+            gender="Male",
+            phone="1234567890",
+            date_of_birth="11 November,1989",
+            subjects="Maths",
+            hobbies="Sports",
+            file_name="20241021_190523.jpg",
+            address="Москва, ул.Ленина, д.1, кв.100",
+            state="NCR",
+            city="Delhi",
+        )
